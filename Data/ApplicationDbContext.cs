e36ad2e93d6c7df0071768509a2f5f11f88285fe
Mayank5054 <mayanksheladiya49@gmail.com>
2024-04-04 16:54:36 +0530

    Ecommerce.DataAccess

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ECommerce.Models;
+
+namespace ECommerce.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opt) : base(opt)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Category>().HasData(
+        //        new Category { CategoryId = 1, Name = "Action", DisplayOrder = 1 },
+        //        new Category { CategoryId = 2, Name = "Sci-Fi", DisplayOrder = 2 },
+        //        new Category { CategoryId = 3, Name = "History", DisplayOrder = 3 }
+        //        );
+        //    base.OnModelCreating(modelBuilder);
+        //}
+    }
+}
