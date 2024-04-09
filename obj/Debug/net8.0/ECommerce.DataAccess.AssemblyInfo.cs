7cf47fc20a35a14540a60be59288c8243de3fae0
Mayank5054 <mayanksheladiya49@gmail.com>
2024-04-09 12:10:12 +0530

    Bug Resolved : Pop Up will Delete The False Product And Category

--- obj/Debug/net8.0/ECommerce.DataAccess.AssemblyInfo.cs
+++ obj/Debug/net8.0/ECommerce.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bafe1930a5f979972b736b28741e2d691c314fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bee095a56af6e42d40055862d6bb215467976bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
